78f2927bfccdf083448350a068a3c346d354a43d
Brice WeKeys <formation@wekeys.fr>
2026-03-23 20:34:20 +0100

    Correction pour Ariane

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get ('/movies', (req, res) => {
     const myURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
     fetch(encodeURI(myURL))
     .then(response => response.json())
-    .then(data => res.json(data.results))
+    .then(data => res.status(200).send({movies: data.results}))
 
 })
 module.exports = router;
