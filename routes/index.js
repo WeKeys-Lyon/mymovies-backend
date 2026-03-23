040cefc534e1b2b2b3090391f8710fa1e52ced30
Brice WeKeys <formation@wekeys.fr>
2026-03-23 20:13:13 +0100

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const apiKey = process.env.API_KEY;
+
+router.get ('/movies', (req, res) => {
+    const myURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
+    fetch(encodeURI(myURL))
+    .then(response => response.json())
+    .then(data => res.json(data.results))
+
+})
+module.exports = router;
